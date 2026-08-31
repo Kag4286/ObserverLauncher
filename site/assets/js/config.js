e4c6cce278ede35c39eadf22b0aa00d9c998e704
ObserverLauncher <dev@observerlauncher.local>
2026-08-31 16:05:24 +0700

    feat: add auto-update and update website

--- site/assets/js/config.js
+++ site/assets/js/config.js
@@ -1,10 +1,10 @@
 // ObserverLauncher site configuration
 export const SITE_CONFIG = {
-  github: 'https://github.com/ObserverLauncher/ObserverLauncher',
-  version: 'v0.1.0-alpha',
+  github: 'https://github.com/Kag4286/ObserverLauncher',
+  version: 'v0.1.0',
   releases: {
-    win: '#download',
-    appimage: '#download',
-    deb: '#download'
+    win: 'https://github.com/Kag4286/ObserverLauncher/releases/download/v0.1.0/ObserverLauncher-0.1.0-setup.exe',
+    appimage: 'https://github.com/Kag4286/ObserverLauncher/releases/download/v0.1.0/ObserverLauncher-0.1.0.AppImage',
+    deb: 'https://github.com/Kag4286/ObserverLauncher/releases/download/v0.1.0/ObserverLauncher-0.1.0.AppImage'
   }
 };
